2e63201e60d548f5c0c3b4bad018e9421bc31c13
sudharsanmaran <smsudharsanmaran>
2022-04-20 13:24:33 +0530

    pep 8

--- products/views.py
+++ products/views.py
@@ -9,41 +9,46 @@ from products.models import Products, User
 from products.serializers import ProductSerializer
 
 
+
+
 class ProductViewSet(viewsets.ViewSet):
+
     def list(self,request):
         products = Products.objects.all()
-        serializer = ProductSerializer(products,many=True)
+        serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
-    def create(self,request):
+    def create(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data,
                         status=status.HTTP_201_CREATED)
 
-    def retrive(self,request, pk=None):
+    def retrive(self, request, pk=None):
         product = Products.objects.get(id=pk)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
 
-    def update(self,request,pk=None):
+    def update(self, request, pk=None):
         product = Products.objects.get(id=pk)
         serializer = ProductSerializer(instance=product,
                                        data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
-    def destroy(self,request,pk=None):
-        product=Products.objects.get(id=pk)
+    def destroy(self, request, pk=None):
+        product = Products.objects.get(id=pk)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
 class UserAPIView(APIView):
-    def get(self,request):
-        users=User.objects.all()
-        user=random.choice(users)
+
+    def get(self, request):
+        users = User.objects.all()
+        user = random.choice(users)
         return Response({
             'id': user.id
         })
